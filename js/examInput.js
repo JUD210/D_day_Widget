7b51f1b6ccbf033eef5ccd84350742dc767d26de
JUD210_Hyuk <judicious210@gmail.com>
2019-02-23 12:57:48 +0900

    Add> exam_data input form

--- js/examInput.js
+++ js/examInput.js
@@ -1,17 +1,35 @@
-let examInputs = [];
+let counter = 0;
 
 function addExamInput() {
   let examInput = document.createElement("div");
-  examInput.classList = "examInput_" + examInputs.length;
+  examInput.classList.add("examInput");
 
   examInput.innerHTML = `
-    Exam + examInputs.length +  <input type='button' value='test'>
+    Exam ${counter} &nbsp;&nbsp;&nbsp;
+    <input class='remove' type='button' value='-' onclick='removeExamInput(this)'>
     <br>
-    
+    Title : &nbsp;&nbsp;
+    <input type="text" name="title" value="" onfocusout="/* 
+    !Todo: 
+    1. send data to json or db!
+    2. reload widget_preview
+    */">
+    <br>
+    Date : &nbsp;&nbsp;
+    <input type="date" name="date" value="" onfocusout="/* 
+    !Todo: 
+    1. send data to json or db!
+    2. reload widget_preview
+    */">
     `;
 
   document
     .querySelector(".exam_data > .group_panel > .content")
     .appendChild(examInput);
 
+  counter++;
+}
+
+function removeExamInput(self) {
+  self.parentNode.parentNode.removeChild(self.parentNode);
 }
