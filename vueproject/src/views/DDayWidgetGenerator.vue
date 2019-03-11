f2b20557b9e7cdf0c3e8c137157f0ee934f025de
JUD210_Hyuk <judicious210@gmail.com>
2019-03-11 13:59:31 +0900

    Create> .d_day_widget_preview on DDayWidgetGenerator

--- vueproject/src/views/DDayWidgetGenerator.vue
+++ vueproject/src/views/DDayWidgetGenerator.vue
@@ -9,7 +9,12 @@
     <div class="d_day_widget input">
       <SettingList></SettingList>
       <button @click="saveWidgetData()">Save</button>
-      <button @click="returnSelf()">test</button>
+      <br /><br />
+      <button @click="showResult()">Show</button>
+    </div>
+
+    <div class="d_day_widget_preview">
+      <DDayWidget></DDayWidget>
     </div>
   </div>
 </template>
@@ -20,18 +25,23 @@ import Description from "@/components/d_day_widget/intro/Description.vue"
 import UniqueIdLoader from "@/components/d_day_widget/input/UniqueIdLoader.vue"
 import SettingList from "@/components/d_day_widget/input/SettingList.vue"
 
+// ! TODO: Add DDAY WIDGET and PREVIEW it !!
+import DDayWidget from "@/components/d_day_widget/output/DDayWidget.vue"
+
 import { mapActions } from "vuex"
 
 export default {
   name: "app",
   components: {
     Description,
-    UniqueIdLoader,
 
+    UniqueIdLoader,
     SettingList,
+
+    DDayWidget,
   },
   methods: {
-    returnSelf() {
+    showResult() {
       this.$router.push({
         name: "d-day-widget-show",
         params: { id: this.$store.state.uniqueId },
@@ -40,37 +50,6 @@ export default {
     ...mapActions(["saveWidgetData"]),
   },
 }
-// import { mapState, mapGetters } from "vuex"
-
-// export default {
-//   methods: {
-//     generateWidget(data) {
-//       setInterval(function() {
-//         const KOREAN_TIME = 1000 * 60 * 60 * 9
-
-//         for (let i = 0; i < data["exam"].length; i++) {
-//           let now = new Date().getTime() + KOREAN_TIME
-
-//           let distance = new Date(data["exam"][i]["date"]) - now
-
-//           let d = Math.floor(distance / (1000 * 60 * 60 * 24))
-//           let h = Math.floor(
-//             (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-//           )
-//           let m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-//           let s = Math.floor((distance % (1000 * 60)) / 1000)
-
-//           document.querySelector("#exam_" + i + " > .dday").innerHTML =
-//             "D - " + d + "d " + h + "h " + m + "m " + s + "s "
-//         }
-//       }, 1000)
-//     },
-//   },
-//   computed: {
-//     ...mapGetters(["getWidgetData"]),
-//     ...mapState([""]),
-//   },
-// }
 </script>
 
 <style scoped></style>
