2026cb933b948ffe8b1157d5dd44a5d644a22311
JUD210_Hyuk <judicious210@gmail.com>
2019-03-30 09:32:37 +0900

    Create> BaseBox from BaseToggleBox

---
+++ src/components/base/BaseBox.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="baseToggleBox">
+    <div class="folder">
+      <span class="folder_title">&nbsp;{{ title }}</span>
+    </div>
+
+    <div v-if="isOpen">
+      <slot>DEBUG: fill the content!</slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import "@/assets/css/base/BaseToggleBox.css"
+
+export default {
+  name: "BaseToggleBox",
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      isOpen: true,
+    }
+  },
+  methods: {},
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import store from "./store/store"
 
 Vue.config.productionTip = false
 
+import BaseBox from "@/components/base/BaseBox.vue"
 import BaseToggleBox from "@/components/base/BaseToggleBox.vue"
 import router from "./router"
+
+Vue.component("BaseBox", BaseBox)
 Vue.component("BaseToggleBox", BaseToggleBox)
 
 new Vue({
